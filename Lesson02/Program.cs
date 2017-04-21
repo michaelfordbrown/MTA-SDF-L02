e65dca63920aa4258c0135ea7876f591d9f5f413
Michael Brown <michaelfordbrown@hotmail.com>
2017-04-21 21:45:46 +0100

    Introduction to Object-Oriented Programming - Create A Class

--- Lesson02/Program.cs
+++ Lesson02/Program.cs
@@ -6,10 +6,27 @@ using System.Threading.Tasks;
 
 namespace Lesson02
 {
+    class Rectangle
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double l, double w)
+        {
+            length = l;
+            width = w;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
