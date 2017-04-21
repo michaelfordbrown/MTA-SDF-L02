4a2e5dcb2f4a95993250172f1b074e7d79346011
Michael Brown <michaelfordbrown@hotmail.com>
2017-04-21 21:53:41 +0100

    Create An Object

--- Lesson02/Program.cs
+++ Lesson02/Program.cs
@@ -26,7 +26,9 @@ namespace Lesson02
     {
         static void Main(string[] args)
         {
-
+            Rectangle rect = new Rectangle(10.0, 20.0);
+            double area = rect.GetArea();
+            Console.WriteLine("Area of Rectangle: {0}", area);
         }
     }
 }
