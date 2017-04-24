a1efaaf60f14a3359b1d9a7442e72b55c1e4b1c3
Michael Brown <michaelfordbrown@hotmail.com>
2017-04-24 14:34:06 +0100

    Create Derived Classes

--- Lesson02/Program.cs
+++ Lesson02/Program.cs
@@ -2,37 +2,23 @@
 
 namespace Lesson02
 {
-    struct Point
+
+    class Polygon
     {
-        public double X, Y;
+        public double Length { get; protected set; }
+        public double Width { get; protected set; }
     }
 
-
-    class Rectangle
+    class Rectangle : Polygon
     {
-        private double length;
-        private double width;
-        public double Length
+        public Rectangle(double length, double width)
         {
-            get
-            {
-                return length;
-            }
-            set
-            {
-                length = value;
-            }
+            Length = length;
+            Width = width;
         }
-        public double Width
+        public double GetArea()
         {
-            get
-            {
-                return width;
-            }
-            set
-            {
-                width = value;
-            }
+            return Width * Length;
         }
     }
 
@@ -40,19 +26,11 @@ namespace Lesson02
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.X = 10;
-            p1.Y = 20;
-            Point p2 = p1;
-            p2.X = 100;
-            Console.WriteLine("p1.X = {0}", p1.X);
 
-            Rectangle rect1 = new Rectangle { Length = 10.0, Width = 20.0 };
-
-            Rectangle rect2 = rect1;
-
-            rect2.Length = 100.0;
-            Console.WriteLine("rect1.Length = {0}", rect1.Length);
+            Rectangle rect = new Rectangle(10, 20);
+            Console.WriteLine(
+                "Width={0}, Length={1}, Area={2}",
+                rect.Width, rect.Length, rect.GetArea());
         }
 
     }
