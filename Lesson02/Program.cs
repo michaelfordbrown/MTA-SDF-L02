db883ece31c00035787be057e281643949683b4d
Michael Brown <michaelfordbrown@hotmail.com>
2017-05-04 22:44:15 +0100

    Use The ICOMPARABLE Interface

--- Lesson02/Program.cs
+++ Lesson02/Program.cs
@@ -11,13 +11,34 @@ namespace Lesson02
         }
     }
 
-    class Rectangle : Polygon
+    class Rectangle : Polygon, IComparable
     {
+        public double Length { get; set; }
+        public double Width { get; set; }
         public override void Draw()
         {
             Console.WriteLine("Drawing: Rectangle");
         }
+        public double GetArea()
+        {
+            return Length * Width;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null)
+                return 1;
+            if (!(obj is Rectangle))
+                throw new ArgumentException();
+            Rectangle target = (Rectangle)obj;
+            double diff = this.GetArea() - target.GetArea();
+            if (diff == 0)
+                return 0;
+            else if (diff > 0)
+                return 1;
+            else return -1;
+        }
     }
+
     class Triangle : Polygon
     {
         public override void Draw()
@@ -32,17 +53,11 @@ namespace Lesson02
     {
         static void Main(string[] args)
         {
-
-            List<Polygon> polygons = new List<Polygon>();
-            polygons.Add(new Polygon());
-            polygons.Add(new Rectangle());
-            polygons.Add(new Triangle());
-
-            foreach (Polygon p in polygons)
-            {
-                p.Draw();
-            }
+            Rectangle rect1 = new Rectangle
+            { Length = 10, Width = 20 };
+            Rectangle rect2 = new Rectangle
+            { Length = 100, Width = 200 };
+            Console.WriteLine(rect1.CompareTo(rect2));
         }
-
     }
 }
